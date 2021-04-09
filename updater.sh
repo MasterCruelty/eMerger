dafe44ae66068e22395716ca4302775cb8fc42a1
Roberto <roberto.antoniello@studenti.unimi.it>
2021-04-09 14:53:37 +0200

    add commands
    autoclean and autoremove added

--- updater.sh
+++ updater.sh
@@ -1,6 +1,16 @@
+#!/bin/bash
+echo -e "apt-get update: starting\n"
 sudo apt-get update
-echo "apt-get update: done"
+echo -e "apt-get update: done\n"
 sleep 1
+echo -e "apt-get upgrade: starting\n"
 sudo apt-get upgrade
-echo "apt-get upgrade: done"
+echo -e "apt-get upgrade: done\n"
 sleep 1
+echo -e "autoclean: starting\n"
+sudo apt-get autoclean #this command deletes useless files so the cache will be clean.
+echo -e "autoclean: done\n"
+sleep 1
+echo -e "autoremove: starting\n"
+sudo apt-get autoremove #this command deletes dependencies that now are useless.
+echo -e "autoremove: done\n"
