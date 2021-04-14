d6bcf844db80debfc8a7ede75d4b4aa3df4f13b9
Roberto <72561502+MasterCruelty@users.noreply.github.com>
2021-04-14 13:31:23 +0200

    Update setup.sh
    Added a couple of printf to visualize which statement is executed.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+
 sed -i "4i cat $(pwd)/logo" $(pwd)/updater.sh
 sed -i "5d" $(pwd)/updater.sh
 
+printf "${RED}\nSetup: starting\n${NORMAL}"
+
 EXST=$(cat ~/.bashrc | grep -c "updater.sh")
 if [[ $EXST -ne 0 ]]; then
+    printf "${RED}\nAlias updater already exists.\nYou can run the script by typing ./updater.sh or 'up'\n${GREEN}Setup completed\n${NORMAL}"
     exit 0
 else
     echo "alias up='bash $(pwd)/updater.sh'" >> ~/.bashrc
     chmod +x updater.sh
+    printf "${GREEN}\nAlias 'up' added.\nYou can run the script by typing ./updater.sh or 'up'.\nSetup completed.\n${NORMAL}"
 fi
