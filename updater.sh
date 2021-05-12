ea3f0135a14a37f189e966ecd4f3b062a7036e95
archetipico <edoardoalejandroferrari@gmail.com>
2021-05-12 13:18:24 +0200

    Add features
    Added 'pkg' packet manager and sudo control

--- updater.sh
+++ updater.sh
@@ -1,23 +1,50 @@
 #!/bin/bash
 
 cat ~/.logo
-printf "\n\n\n"
+printf "\n"
 
-RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
 NORMAL=$(tput sgr0)
 
+PKG=""
+SUDO=""
+
 printProgress() {
-    if [[ "$2" == "starting" ]]; then
-	    printf "${RED}\n$1 $2\n${NORMAL}"
+    if [[ "$1" == *starting ]] || [[ "$1" == *privileges ]] || [[ "$1" == *aborting* ]]; then
+	    printf "${RED}\n$1\n${NORMAL}"
     else
-	    printf "${GREEN}$1 $2\n${NORMAL}"
+	    printf "${GREEN}$1\n${NORMAL}"
     fi
 }
 
-printf "${RED}This script requires sudo to run commands.\n${NORMAL}"
-PKG=""
-if [[ -n "$(command -v apt-get)" ]]; then
+printProgress "Checking for sudo privileges"
+sudo -v >/dev/null 2>&1
+if [[ "$(echo $?)" -eq 0 ]]; then
+    printProgress "Access granted.\n"
+    SUDO="sudo"
+else
+    printProgress "Can't access: aborting script.\n"
+    exit 1
+fi
+
+if [[ -n "$(command -v pkg)" ]]; then
+    PKG="pkg"
+
+    printf "${GREEN}System detected: ${RED}Using $PKG\n${NORMAL}"
+
+    printProgress "update: starting"
+    $SUDO $PKG update
+    printProgress "update: completed"
+
+    printProgress "upgrade: starting"
+    $SUDO $PKG upgrade
+    printProgress "upgrade: completed"
+
+    printProgress "autoremove: starting"
+    $SUDO $PKG autoremove
+    printProgress "autoremove: completed"
+elif [[ -n "$(command -v apt-get)" ]]; then
     PKG="apt-get"
     if [[ -n "$(command -v apt)" ]]; then
         PKG="apt"
@@ -25,21 +52,21 @@ if [[ -n "$(command -v apt-get)" ]]; then
     
     printf "${GREEN}System detected: ${RED}Using $PKG\n${NORMAL}"
     
-    printProgress update: starting
-    sudo $PKG update
-    printProgress update: completed
+    printProgress "update: starting"
+    $SUDO $PKG update
+    printProgress "update: completed"
 
-    printProgress upgrade: starting
-    sudo $PKG upgrade
-    printProgress upgrade: completed
+    printProgress "upgrade: starting"
+    $SUDO $PKG upgrade
+    printProgress "upgrade: completed"
 
-    printProgress autoclean: starting
-    sudo $PKG autoclean
-    printProgress autoclean: completed
+    printProgress "autoclean: starting"
+    $SUDO $PKG autoclean
+    printProgress "autoclean: completed"
 
-    printProgress autoremove: starting
-    sudo $PKG autoremove
-    printProgress autoremove: completed
+    printProgress "autoremove: starting"
+    $SUDO $PKG autoremove
+    printProgress "autoremove: completed"
 elif [[ -n "$(command -v yum)" ]]; then
     PKG="yum"
     if [[ -n "$(command -v dnf)" ]]; then
@@ -48,65 +75,67 @@ elif [[ -n "$(command -v yum)" ]]; then
 
     printf "${GREEN}System detected: ${RED}Using $PKG\n${NORMAL}"
 
-    printProgress update: starting
-    sudo $PKG update
-    printProgress update: completed
+    printProgress "update: starting"
+    $SUDO $PKG update
+    printProgress "update: completed"
 
-    printProgress upgrade: starting
-    sudo $PKG upgrade
-    printProgress upgrade: completed
+    printProgress "upgrade: starting"
+    $SUDO $PKG upgrade
+    printProgress "upgrade: completed"
 
-    printProgress cleanAll: starting
-    sudo $PKG clean all
-    printProgress cleanAll: completed
+    printProgress "cleanAll: starting"
+    $SUDO $PKG clean all
+    printProgress "cleanAll: completed"
 elif [[ -n "$(command -v pacman)" ]]; then
     PKG="pacman"
 
     printf "${GREEN}System detected: ${RED}Using $PKG\n${NORMAL}"
 
-    printProgress update: starting
-    sudo $PKG -Syy
-    printProgress update: completed
+    printProgress "update: starting"
+    $SUDO $PKG -Syy
+    printProgress "update: completed"
 
-    printProgress upgrade: starting
-    sudo $PKG -Syu
-    printProgress upgrade: completed
+    printProgress "upgrade: starting"
+    $SUDO $PKG -Syu
+    printProgress "upgrade: completed"
 
-    printProgress cleanAll: starting
-    sudo $PKG -R $($PKG -Qtdq)
-    printProgress cleanAll: completed
+    printProgress "cleanAll: starting"
+    $SUDO $PKG -R $($PKG -Qtdq)
+    printProgress "cleanAll: completed"
 elif [[ -n "$(command -v emerge)" ]]; then
     PKG="emerge"
 
     printf "${GREEN}System detected: ${RED}Using $PKG\n${NORMAL}"
 
-    printProgress syncing: starting
+    printProgress "syncing: starting"
     $PKG --sync
-    printProgress syncing: completed
+    printProgress "syncing: completed"
 
-    printProgress update: starting
+    printProgress "update: starting"
     $PKG --update --deep --newuse --with-bdeps y @world --ask
-    printProgress update: completed
+    printProgress "update: completed"
 
-    printProgress deepclean: starting
+    printProgress "deepclean: starting"
     emerge --depclean --ask
     revdep-rebuild
-    printProgress deepclean: completed
+    printProgress "deepclean: completed"
 else
     printf "${RED}System not supported${NORMAL}"
 fi
 
-if [[ -d "~/.local/share/Trash/files" ]]; then
-	printf "${RED}\nShowing files in .local/share/Trash/${NORMAL}\n"
+if [[ -d ~/.local/share/Trash/files ]]; then
+	printf "${RED}\nShowing files in .local/share/Trash/files${NORMAL}\n"
 	ls -hl ~/.local/share/Trash/files
 	printf "Should I clean Trash? "
 	read -p "[y/n]: " ANSW 
 	if [[ "$ANSW" == "y" ]]; then
-	    sudo rm -rf ~/.local/share/Trash/*
-	    printProgress Trash: "cleaned"
+	    $SUDO rm -rf ~/.local/share/Trash/*
+	    printProgress "Trash: cleaned"
 	else
-	    printProgress Trash: "not cleaned"
+	    printProgress "Trash: not cleaned"
 	fi
+else
+    printProgress "\nTrash is empty, nothing to clean."
 fi
 
 printf "\n"
