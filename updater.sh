846b785ac1377da90b77a3d6e1c1e15b3e93b0be
Roberto <roberto.antoniello@studenti.unimi.it>
2021-05-12 14:16:55 +0200

    refactor updater.sh
    Deleted useless line.

--- updater.sh
+++ updater.sh
@@ -59,7 +59,6 @@ printProgress "Checking for sudo privileges"
 sudo -v >/dev/null 2>&1
 if [[ "$(echo $?)" -eq 0 ]]; then
     printProgress "Access granted.\n"
-    SUDO="sudo"
 else
     printProgress "Can't access: aborting script.\n"
     exit 1
