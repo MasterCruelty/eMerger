7a7a6b72b6c7cef62097a4bad620bca495740a89
Roberto <roberto.antoniello@studenti.unimi.it>
2021-05-13 12:09:20 +0200

    Fix uninstall.sh
    Moved a couple of printf a line later after the commands are actually executed

--- uninstall.sh
+++ uninstall.sh
@@ -3,10 +3,10 @@
 RED=$(tput setaf 1)
 NORMAL=$(tput sgr0)
 
-printf "${RED}File '.logo' removed ${NORMAL}"
 rm ~/.logo
-printf "${RED}Alias 'up' removed\n${NORMAL}"
+printf "${RED}File '.logo' removed ${NORMAL}"
 sed -i "/alias up=/d" ~/.bashrc
+printf "${RED}Alias 'up' removed\n${NORMAL}"
 
 printf "${RED}\nUninstallation completed\n${NORMAL}"
 
