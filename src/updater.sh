88e61db7633b06cf3156fcb35aefd1421f0ba492
archetipico <edoardoalejandroferrari@gmail.com>
2021-06-23 17:35:52 +0200

    Change variable name
    Changed variable 'src_path' name and simplified code

--- src/updater.sh
+++ src/updater.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-src_path=$(dirname "$(readlink -f "$0")")
-source "$src_path"/utils/global.sh
+SRC=$(dirname "$(readlink -f "$0")")
+source $SRC/utils/global.sh
 
-if [[ -f "$src_path/utils/.cache" ]]; then
-	HASH=$(md5sum "$src_path/utils/.cache" | cut -d " " -f1)
-	if [[ $HASH != $(cat $src_path/utils/.md5) ]]; then
-		md5sum "$src_path"/utils/.cache | cut -d " " -f1 > "$src_path"/utils/.md5
+if [[ -f "$SRC/utils/.cache" ]]; then
+	HASH=$(md5sum "$SRC/utils/.cache" | cut -d " " -f1)
+	if [[ $HASH != $(cat $SRC/utils/.md5) ]]; then
+		md5sum $SRC/utils/.cache | cut -d " " -f1 > $SRC/utils/.md5
 	fi
 else
-    "$src_path"/utils/cachegen.sh > "$src_path"/utils/.cache
-    md5sum "$src_path"/utils/.cache | cut -d " " -f1 > "$src_path"/utils/.md5
-	chmod 775 "$src_path"/utils/.cache
+    $SRC/utils/cachegen.sh > $SRC/utils/.cache
+    md5sum $SRC/utils/.cache | cut -d " " -f1 > $SRC/utils/.md5
+	chmod 775 $SRC/utils/.cache
 fi
-chmod 775 "$src_path"/utils/.md5
+chmod 775 $SRC/utils/.md5
 
-printf "${LOGO}"
+printf "$LOGO"
 if [[ $(stty size | awk '{print $2}') -ge 69 ]]; then
-	cat "$src_path"/utils/.logo
+	cat $SRC/utils/.logo
 fi
-printf "Running on: $(uname -rs)\n${NORMAL}"
+printf "Running on: $(uname -rs)\n$NORMAL"
 
 while read line; do
 	if [[ "$line" != "" ]]; then
-        source "$src_path"/"$line".sh;
+        source $SRC/$line.sh;
     fi
-done < $src_path/utils/.cache
+done < $SRC/utils/.cache
 
 exit 0
