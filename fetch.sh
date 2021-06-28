4556490bde2bd8cd0c9a7165dac09c9294cc96bb
Roberto <robertino.antoniello@gmail.com>
2021-06-28 23:01:47 +0200

    Added comment

--- fetch.sh
+++ fetch.sh
@@ -2,10 +2,12 @@
 
 source src/utils/global.sh
 
+#I do the git pull from the repository
 printProgress "Fetching repository: starting"
 git pull  https://www.github.com/MasterCruelty/Updater
 printProgress "Fetching repository: done\n"
 
+#I launch uninstall and setup
 printProgress "Uninstalling old version and setup: starting"
 bash uninstall.sh "fetch"  
 bash setup.sh "fetch"
