1b38648f1bd30c3708ee69728b7f8564a6f2009e
archetipico <edoardoalejandroferrari@gmail.com>
2021-07-05 00:05:19 +0200

    Add error tracking
    Tracking errors when running setup.sh and uninstall.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-source src/utils/global.sh
+source src/utils/global.sh 2>>.errors
 printProgress "Setup: starting"
 
 EXST=$(cat ~/.bashrc | grep -c "emerger.sh")
 if [[ $EXST -ne 0 ]]; then
     printProgress "Alias 'up' already exists. Use 'up' or run './src/emerger.sh'"
-    source src/test/integrity_check.sh
+    source src/test/integrity_check.sh 2>>.errors
     printProgress "Setup: completed $COOL"
 else
-    source src/utils/cache_gen.sh > src/utils/.cache
-    md5sum src/utils/.cache | cut -d " " -f1 > src/utils/.md5
-    chmod 775 src/utils/.cache
-    chmod 775 src/utils/.md5
+    source src/utils/cache_gen.sh > src/utils/.cache 2>>.errors
+    md5sum src/utils/.cache | cut -d " " -f1 > src/utils/.md5 2>>.errors
+    chmod 775 src/utils/.cache 2>>.errors
+    chmod 775 src/utils/.md5 2>>.errors
     
     echo "alias up='bash $(pwd)/src/emerger.sh'" >> ~/.bashrc
-    chmod +x src/emerger.sh
+    chmod +x src/emerger.sh 2>>.errors
     printProgress "Alias 'up' added.\nUse 'up' or run './src/emerger.sh'"
-    source src/test/integrity_check.sh
+    source src/test/integrity_check.sh 2>>.errors
     printProgress "Setup: completed $COOL"
 fi
 
@@ -31,7 +31,7 @@ if [[ $1 != "fetch" ]]; then
         read -p "Press enter, this process will be killed" answ
         printf "${NORMAL}"
         
-        $TERMINAL
+        $TERMINAL 2>>.errors
         kill -9 $PPID
     fi
 fi
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source src/utils/global.sh
+source src/utils/global.sh 2>>.errors
 
 if [[ $(grep -c "alias up=" ~/.bashrc) -lt 1 ]]; then
     printf "${GREEN}Nothing to uninstall $CHECKMARK${NORMAL}\n"
@@ -8,15 +8,15 @@ if [[ $(grep -c "alias up=" ~/.bashrc) -lt 1 ]]; then
 fi
 
 printProgress "Uninstall: starting"
-sed -i "/alias up=/d" ~/.bashrc
+sed -i "/alias up=/d" ~/.bashrc 2>>.errors
 printf "${RED}Alias 'up' removed${NORMAL}\n"
 printProgress "Uninstall: completed $SAD"
 
 if [[ $1 != "fetch" ]]; then
     TERMINAL=$(cat src/utils/.cache | head -n 2 | tail -n 1)
 
-    rm -f src/utils/.cache 2>/dev/null
-    rm -f src/utils/.md5 2>/dev/null
+    rm -f src/utils/.cache 2>>.errors
+    rm -f src/utils/.md5 2>>.errors
 
     if [[ $TERMINAL == "unknown" ]]; then
         exec bash
@@ -26,7 +26,7 @@ if [[ $1 != "fetch" ]]; then
         read -p "Press enter, this process will be killed" answ
         printf "${NORMAL}"
         
-        $TERMINAL
+        $TERMINAL 2>>.errors
         kill -9 $PPID
     fi
 fi
