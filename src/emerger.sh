f7e581fe0e5bedb4e84e789459f35ee7032e5e86
archetipico <edoardoalejandroferrari@gmail.com>
2021-07-13 23:11:39 +0200

    Fix bug
    Fixed UI bug

--- src/emerger.sh
+++ src/emerger.sh
@@ -33,24 +33,27 @@ else
     fi
     chmod 775 $SRC/utils/.md5
 
+    # Logo
     if [[ ! $ARGV =~ "-nl" ]]; then
         printf "$LOGO"
         if [[ $(stty size | awk '{print $2}') -ge 74 ]]; then
             cat $SRC/utils/.logo
         fi
-        printf "Contribute @ https://github.com/MasterCruelty/eMerger $WHALE\n$NORMAL"
+        printf "Contribute @ https://github.com/MasterCruelty/eMerger $WHALE$NORMAL\n"
     fi
 
+    # System informations
     if [[ ! $ARGV =~ "-ni" ]]; then
         printf "${LOGO}Running on: "
         if [[ -f "/etc/os-release" ]]; then
             NAME=$(cat /etc/os-release | head -n $(echo $(grep -n "PRETTY_NAME" /etc/os-release) | cut -c 1) | tail -n 1 | cut -c 14-)
-            printf "${NAME::-1}\n"
+            printf "${NAME::-1}$NORMAL\n"
         else
-            printf "$(uname -rs)\n$NORMAL"
+            printf "$(uname -rs)$NORMAL\n"
         fi
     fi
 
+    # Weather
     if [[ $ARGV =~ "-w" ]]; then
         # Using wttr.in to show the weather using the following arguments:
         # %l = location; %c = weather emoji; %t = actual temp; %w = wind km/h; %m = Moon phase
