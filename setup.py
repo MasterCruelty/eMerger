fb2d0178f1912bf561d370a20e8c3fbb0d3a19b4
archetipico <43927507+archetipico@users.noreply.github.com>
2024-04-17 00:04:14 +0200

    Refactor

--- setup.py
+++ setup.py
@@ -1,24 +1,33 @@
 from colorama import init, Fore
 from platform import system
-from utils import check_packages, update_cache
+from utils import Utils
 
-# Initialize colorama
-init()
+if __name__ == '__main__':
+    # Initialize colorama
+    init()
+    print('{}----- SETUP PROCESS -----{}'.format(Fore.GREEN, Fore.RESET))
 
-# Check system
-match system():
-    case 'Darwin':
-         # Update system in cache
-        update_cache({'system': 'darwin'})
-        print('{}>{} System set to Darwin'.format(Fore.BLUE, Fore.RESET))
-        check_packages()
-    case 'Linux':
-        # Update system in cache
-        update_cache({'system': 'linux'})
-        print('{}>{} System set to Linux'.format(Fore.BLUE, Fore.RESET))
-        check_packages()
-    case 'Windows':
-        # Update system in cache
-        update_cache({'system': 'windows'})
-        print('{}>{} System set to Windows'.format(Fore.BLUE, Fore.RESET))
-        check_packages()
+    # Check system
+    utils = Utils()
+    match system():
+        case 'Darwin':
+            # Update system in cache
+            utils.update_cache({'system': 'darwin'})
+            print('{}>{} System set to Darwin'.format(Fore.BLUE, Fore.RESET))
+            # Update packages
+            utils.check_packages()
+        case 'Linux':
+            # Update system in cache
+            utils.update_cache({'system': 'linux'})
+            print('{}>{} System set to Linux'.format(Fore.BLUE, Fore.RESET))
+            # Update packages
+            utils.check_packages()
+        case 'Windows':
+            # Update system in cache
+            utils.update_cache({'system': 'windows'})
+            print('{}>{} System set to Windows'.format(Fore.BLUE, Fore.RESET))
+            # Update packages
+            utils.check_packages()
+        case _:
+            print('{}> THE SYSTEM IS NOT SUPPORTED: ABORTING SETUP{}'.format(Fore.RED, Fore.RESET))
+            exit()
